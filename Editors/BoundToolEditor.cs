c72f57332693b46aa947489259aaf8d6d0745bf2
XiaoTN_company <1136716949@qq.com>
2024-01-02 14:04:14 +0800

    添加Editor工具类

--- Editors/BoundToolEditor.cs
+++ Editors/BoundToolEditor.cs
@@ -1,7 +1,22 @@
+using UnityEditor;
+using UnityEngine;
+using XTN.BoundTool;
+
 namespace BoundToolPackage.XTNEditor
 {
-    public static class BoundToolEditor
+    public class BoundToolEditor: Editor
     {
-        
+        [MenuItem("GameObject/XTNTool/Bound/AddCollider", false,0)]
+        public static void AddCollider()
+        {
+            GameObject go = Selection.gameObjects[0];
+            if (go == null)
+            {
+                return;
+            }
+
+            BoundsUtils.AddBoxCollider(go.transform);
+            EditorUtility.SetDirty(go);
+        }
     }
 }
